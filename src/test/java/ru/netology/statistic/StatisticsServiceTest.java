0a608f646b98dc79afcb864ba6411cd4443f448f
Fantasma <kadr-90@mail.ru>
2025-02-16 19:45:48 +0700

    Add tests for full coverage v2

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,6 +17,7 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     void findMaxWithAllEqualValues() {
         StatisticsService service = new StatisticsService();
